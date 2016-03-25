001ded27fcc570f5d844994785b586b82bdb7bbc
nicolise <ne.soltis@gmail.com>
2016-03-24 17:29:53 -0700

    Working on tab to binary csv

--- scripts/NESbigRR/TABtobinCSV.R
+++ scripts/NESbigRR/TABtobinCSV.R
@@ -18,6 +18,7 @@ setwd("~/Documents/GitRepos/BcSolGWAS/data/SNP_files")
 
 SNPsMAF20 <- read.csv("snps_maf20.csv")
 mySNPs <- SNPsMAF20
+mySNPs <- miniSNPs
 #convert N/N format to N
 str(mySNPs)
 #make these characters instead of factors
@@ -26,18 +27,55 @@ mySNPs[mySNPs=="C/C"]<-"C"
 mySNPs[mySNPs=="T/T"]<-"T"
 mySNPs[mySNPs=="A/A"]<-"A"
 mySNPs[mySNPs=="G/G"]<-"G"
+mySNPs[mySNPs=="A/C"]<-"NA"
+mySNPs[mySNPs=="A/G"]<-"NA"
+mySNPs[mySNPs=="A/T"]<-"NA"
+mySNPs[mySNPs=="C/A"]<-"NA"
+mySNPs[mySNPs=="C/G"]<-"NA"
+mySNPs[mySNPs=="C/T"]<-"NA"
+mySNPs[mySNPs=="G/A"]<-"NA"
+mySNPs[mySNPs=="G/C"]<-"NA"
+mySNPs[mySNPs=="G/T"]<-"NA"
+mySNPs[mySNPs=="T/A"]<-"NA"
+mySNPs[mySNPs=="T/C"]<-"NA"
+mySNPs[mySNPs=="T/G"]<-"NA"
 mySNPs[mySNPs=="./."]<-"NA"
 
+unlist(unique(mySNPs$X305))
+table(mySNPs$X305)
+
+mySNPsMAF20 <- mySNPs
+miniMAF20 <- mySNPsMAF20[1:20,1:20]
+mySNPs <- miniMAF20
 #17 possible states: A/A G/G T/T C/C G/A T/C A/T C/T T/A A/G G/T C/A A/C ./. G/C T/G C/G
 #how to deal with heterozygosity?
 
 #replace base with 1 if match
 #loop through it
-for (i in names(mySNPs[4:18])) {
-  mySNPs[i][mySNPs[i]!=mySNPs$REF] <- 1
+#but how to do NA?
+for (i in names(mySNPs[4:20])) {
+  mySNPs[i][mySNPs[i]!=mySNPs$REF & is.na(mySNPs[i])=F] <- 1
   mySNPs[i][mySNPs[i]==mySNPs$REF] <- 0
 }
 
+is.na
+#if else
+for (i in names(mySNPs[4:18])) {
+  if (mySNPs[i]==NA){
+    [mySNPs[i] <- NA}else 
+      if (mySNPs[i]!=mySNPs$REF) {
+        mySNPs[i]<- 1}else 
+          if (mySNPs[i]==mySNPs$REF) {
+            mySNPs[i]<- 0}
+}
+
+if (mySNPs[4]==NA){
+  [mySNPs[4] <- NA}else 
+    if (mySNPs[4]!=mySNPs$REF) {
+      mySNPs[4]<- 1}else 
+        if (mySNPs[4]==mySNPs$REF) {
+          mySNPs[4]<- 0}
+}
 #-------------------------------------------------------------
 #junk code
 #other ideas
