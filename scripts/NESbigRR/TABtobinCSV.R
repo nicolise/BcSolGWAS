1211ad2cab46b7e106bcb018ab5b4cec30e99ce7
Nicole E Soltis <ne.soltis@gmail.com>
2016-03-25 14:11:27 -0700

    ready to convert files

--- scripts/NESbigRR/TABtobinCSV.R
+++ scripts/NESbigRR/TABtobinCSV.R
@@ -2,23 +2,29 @@
 #convert .tab SNP data to binary .csv
 
 #------------------------------------------------------
+#on linux desktop
 setwd("~/Documents/GitRepos/BcSolGWAS/data/SNP_files")
+#on laptop
+setwd("~/Projects/BcSolGWAS/data/genome")
 #practice with mini file first
 #miniSNPs <- read.csv("minisnps.csv")
+#miniSNPs <- read.csv("miniMAF20.csv")
+#mySNPs <- miniSNPs
 
-#convert both .tab SNP files to .csv
-#tab10 = read.delim("snps_maf10.tab")
-#write.table(tab10, file="snps_maf10.csv",sep=",",col.names=T,row.names=FALSE)
-
+#convert both all .tab SNP files to .csv
 #tab5 = read.delim("snps_maf5.tab")
 #write.table(tab5, file="snps_maf5.csv",sep=",",col.names=T,row.names=FALSE)
-
+#tab10 = read.delim("snps_maf10.tab")
+#write.table(tab10, file="snps_maf10.csv",sep=",",col.names=T,row.names=FALSE)
 #tab20 = read.delim("snps_maf20.tab")
 #write.table(tab20, file="snps_maf20.csv",sep=",",col.names=T,row.names=FALSE)
 
+#convert each file to binary
+#SNPsMAF5 <- read.csv("snps_maf5.csv")
+#SNPsMAF10 <- read.csv("snps_maf10.csv")
 SNPsMAF20 <- read.csv("snps_maf20.csv")
 mySNPs <- SNPsMAF20
-mySNPs <- miniSNPs
+
 #convert N/N format to N
 str(mySNPs)
 #make these characters instead of factors
@@ -40,44 +46,27 @@ mySNPs[mySNPs=="T/A"]<-"NA"
 mySNPs[mySNPs=="T/C"]<-"NA"
 mySNPs[mySNPs=="T/G"]<-"NA"
 mySNPs[mySNPs=="./."]<-"NA"
+allSNPs<- mySNPs
+#17 possible states: A/A G/G T/T C/C G/A T/C A/T C/T T/A A/G G/T C/A A/C ./. G/C T/G C/G
+#how to deal with heterozygosity?
 
 unlist(unique(mySNPs$X305))
 table(mySNPs$X305)
 
-mySNPsMAF20 <- mySNPs
-miniMAF20 <- mySNPsMAF20[1:20,1:20]
-mySNPs <- miniMAF20
-#17 possible states: A/A G/G T/T C/C G/A T/C A/T C/T T/A A/G G/T C/A A/C ./. G/C T/G C/G
-#how to deal with heterozygosity?
+#mySNPsMAF20 <- mySNPs
+#miniMAF20 <- mySNPsMAF20[1:20,1:20]
+#mySNPs <- miniMAF20
 
 #replace base with 1 if match
 #loop through it
-#but how to do NA?
-for (i in names(mySNPs[4:20])) {
-  mySNPs[i][mySNPs[i]!=mySNPs$REF & is.na(mySNPs[i])=F] <- 1
+#automatically skips NAs
+for (i in names(mySNPs[5:21])) {
+  mySNPs[i][mySNPs[i]!=mySNPs$REF]  <- 1
   mySNPs[i][mySNPs[i]==mySNPs$REF] <- 0
 }
 
-is.na
-#if else
-for (i in names(mySNPs[4:18])) {
-  if (mySNPs[i]==NA){
-    [mySNPs[i] <- NA}else 
-      if (mySNPs[i]!=mySNPs$REF) {
-        mySNPs[i]<- 1}else 
-          if (mySNPs[i]==mySNPs$REF) {
-            mySNPs[i]<- 0}
-}
 
-if (mySNPs[4]==NA){
-  [mySNPs[4] <- NA}else 
-    if (mySNPs[4]!=mySNPs$REF) {
-      mySNPs[4]<- 1}else 
-        if (mySNPs[4]==mySNPs$REF) {
-          mySNPs[4]<- 0}
-}
-
-write.csv(miniMAF20, "miniMAF20.csv")
+write.csv(mySNPs, "binaryMAF20.csv")
 #-------------------------------------------------------------
 #junk code
 #other ideas
@@ -91,3 +80,15 @@ apply(within(miniSNPs, colnames(miniSNPs)))
 testing <- within(miniSNPs, newvar <- 1*(i == REF))
 mytest <- with(miniSNPs, ifelse(i == REF, 0, 1))
 tapply(summaryvar, groupvar, function)
+
+#doesn't work
+is.na
+#if else
+for (i in names(mySNPs[4:18])) {
+  if (mySNPs[i]==NA){
+    [mySNPs[i] <- NA}else 
+      if (mySNPs[i]!=mySNPs$REF) {
+        mySNPs[i]<- 1}else 
+          if (mySNPs[i]==mySNPs$REF) {
+            mySNPs[i]<- 0}
+}
