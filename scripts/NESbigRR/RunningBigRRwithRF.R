fd0155d3a89f16fcd6fbaa91a95c7a5595a16c19
nicolise <ne.soltis@gmail.com>
2016-04-29 18:15:04 -0700

    generated SL lesion size manhattan plots. Need to troubleshoot: effect sizes seem large. And chromosome indices are overlapping.

--- scripts/NESbigRR/RunningBigRRwithRF.R
+++ scripts/NESbigRR/RunningBigRRwithRF.R
@@ -4,7 +4,7 @@
 #---------------------------------------------------------------
 
 rm(list=ls())
-setwd("~/Documents/GitRepos/BcSolGWAS/data/SNP_files/bigRRinput")
+setwd("~/Documents/GitRepos/BcSolGWAS/")
 
 #########################
 # This makes the bigRR_update run through the GPU
@@ -111,9 +111,6 @@ for(i in 1:dim(dat)[2]) { #i will be each isolate
   thresh.HEM$"0.999Thresh"[i] <- quantile(perm.u.HEM,0.999)
   colnames(outpt.BLUP)[i+1] <- paste(colnames(dat)[i],"BLUP",sep=".")
   colnames(outpt.HEM)[i+1] <- paste(colnames(dat)[i],"HEM",sep=".")
-  
-  
-  
 }
 
 #Give column names to the thresholds from the HEM list
@@ -185,25 +182,15 @@ screen(4); plot(abs(Pheno.HEM.result$leverage), cex = .6, col = 'olivedrab')
 library(ggplot2)
 library(grid)
 
-#Import data
-HEM.plotdata <- read.csv("Sl_LesionSizePoisson.HEM.PlotFormat.csv")#RF-this is just a reorganized file of LesionSize.HEM.csv.
-#RF-Basically it has two columns containing Chrom and pos separately instead of just one column with eg "III.57894". This is easiest to do with code below:
-#library(tidyr)
-#separate(old data, V1, into = c("Chrom", "Pos"))
-#but tidyr requires a more recent version of R to run, so you must take the file to a different comp to do this
+#Import data (reorganized from script ReformatBigRRouts.R)
+HEM.plotdata <- read.csv("data/GWAS_files/04_bigRRoutput/Sl_LesionSize.HEM.PlotFormat.csv")
 
 HEM.plotdata$Pos <- as.character(HEM.plotdata$Pos)#ensure that position data is not in scientific notation
-HEM.plotdata <- HEM.plotdata[-c(1:4), -c(1:2)]
+HEM.plotdata <- HEM.plotdata[-c(1:2)]
 
-#RF-Extract thresholds - 4 rows of 4 threshhold values, we just want the result of nonpermuted data to plot
-#HEM.Perm.Threshold <- as.vector(HEM.plotdata[1,])
-#HEM.plotdata <- HEM.plotdata[-1:4,] #already did this 
-
-
-#get threshhold values from HEM file 
-#make new file with only top 5 rows of Sl_LesionSizePoisson.HEM.csv
-HEM.thresh <- read.csv("Sl_LesionSizePoisson.HEM.Thresh.csv")
-HEM.thresh <- HEM.thresh[,-1]
+#get threshhold values 
+HEM.thresh <- read.csv("data/GWAS_files/04_bigRRoutput/Sl_LesionSize.HEM.Thresh.csv")
+HEM.thresh <- HEM.thresh[,-c(1:2)]
 TH99 <- HEM.thresh[3,]
 TH99_LA0410 <- as.numeric(TH99[2])
 TH99_LA0480 <- as.numeric(TH99[3])
@@ -252,17 +239,10 @@ HEM.plotdata$Chrom <- gsub("Chromosome", "", HEM.plotdata$Chrom)
 HEM.plotdata$Chrom <- as.numeric(as.character(HEM.plotdata$Chrom))
 HEM.plotdata$Pos <- as.numeric(as.character(HEM.plotdata$Pos))
 
-
-#Give column names to the thresholds from the HEM list
-#for(j in 1:length(thresh.HEM)) {
-#  names(thresh.HEM[[j]]) <- colnames(dat)
-#}
-#RF - we already ran this command above
-
 #Make plotting variables
-HEM.plotdata$Index=NA
+HEM.plotdata$Index = NA
 ticks = NULL
-lastbase=0
+lastbase = 0
 
 ##NES failing here
 #Redo the positions to make them sequencial		-- accurate position indexing
@@ -280,7 +260,7 @@ for (i in unique(HEM.plotdata$Chrom)) {
 ticklim=c(min(HEM.plotdata$Index),max(HEM.plotdata$Index))
 
 #make plots for each phenotype
-jpeg("Sl_LesionSize_LA0410.ManhattanPlot.jpg")
+jpeg("plots/Sl_LesionSize_LA0410.ManhattanPlot.jpg")
 qplot(Index,abs(LA0410), data=HEM.plotdata, ylab="SNP Effect Estimate" , 
       main = "LesionSize_LA0410", colour=factor(Chrom)) +
  geom_hline(yintercept=TH99_LA0410) +
@@ -289,29 +269,34 @@ geom_hline(yintercept=TH95_LA0410, colour = "blue") +
   geom_text(aes(0,TH95_LA0410, label = ".95 Threshold", vjust = 1.5, hjust = .05), col = "blue")
 dev.off()
 
-#how many SNPs are above a certain threshhold?
-sum(HEM.plotdata$Lesion.0.m.eccentricity_Apple517 >= TH99_Apple517) #spits out number
-#Eccentricity = 597, Area = 1709, Grn = 1573
-
-#try ggplot? - must add "environment" argument - code below doesn't work yet
-# ggplot(data=HEM.plotdata, aes(HEM.plotdata$Index, abs(Lesion.0.m.eccentricity_Apple517)), ylab="SNP Effect Estimate" , 
-#        main = "Lesion.0.m.eccentricity_Apple517", colour=factor(Chrom)) +
-#  geom_hline(aes(yintercept=TH99))
-
-sum(HEM.plotdata$Lesion.0.m.eccentricity_B05.10 >= TH99_B05.10) #spits out number
-
-jpeg("Sl_LesionSize_LA2176.ManhattanPlot.jpg")
-qplot(Index,abs(LA2176), data=HEM.plotdata, ylab="SNP Effect Estimate" , 
-      main = "LesionSize_LA2176", colour=factor(Chrom)) +
-  geom_hline(yintercept=TH99_LA2176) +
-  geom_text(aes(0,TH99_LA2176, label = ".99 Threshold", vjust = 1.5, hjust = .05), col = "black") +
-  geom_hline(yintercept=TH95_LA2176, colour = "blue") +
-  geom_text(aes(0,TH95_LA2176, label = ".95 Threshold", vjust = 1.5, hjust = .05), col = "blue")
+jpeg("plots/Sl_LesionSize_LA0410.ManhattanPlot_999.jpg")
+qplot(Index,abs(LA0410), data=HEM.plotdata, ylab="SNP Effect Estimate" , 
+      main = "LesionSize_LA0410", colour=factor(Chrom)) +
+  geom_hline(yintercept=TH999_LA0410) +
+  geom_text(aes(0,TH999_LA0410, label = ".999 Threshold", vjust = 1.5, hjust = .05), col = "black")
 dev.off()
 
-#make plot with all phenos?
+#how many SNPs are above a certain threshhold?
+sum(HEM.plotdata$LA0410 >= TH999_LA0410) #spits out number
+#6491
 
+#rest of plots
+jpeg("plots/Sl_LesionSize_LA4355.ManhattanPlot_99.jpg")
+qplot(Index,abs(LA4355), data=HEM.plotdata, ylab="SNP Effect Estimate" , 
+      main = "LesionSize_LA4355", colour=factor(Chrom)) +
+  geom_hline(yintercept=TH99_LA4355, colour = "black") +
+  geom_text(aes(0,TH99_LA4355, label = ".99 Threshold", vjust = 1.5, hjust = .05), col = "black")
+dev.off()
 
+#not sure why chromosomes are overlapping
+m <- ggplot(HEM.plotdata, aes(x=Index, fill=as.factor(Chrom)))
+m + geom_bar() 
+
+#indices ARE overlapping
+range(subset(HEM.plotdata,HEM.plotdata$Chrom==1)$Index)
+range(subset(HEM.plotdata,HEM.plotdata$Chrom==2)$Index)
+range(subset(HEM.plotdata,HEM.plotdata$Chrom==12)$Index)
+range(subset(HEM.plotdata,HEM.plotdata$Chrom==13)$Index)
 
 #set my colors
 mycols=rep(c("gray10","gray60"),max(HEM.plotdata$Chrom))
