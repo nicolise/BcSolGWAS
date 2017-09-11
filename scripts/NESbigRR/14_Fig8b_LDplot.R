3f89dbabb9b11337656826b7ccc722d3ab15e4c4
Nicole E Soltis <ne.soltis@gmail.com>
2017-09-11 15:13:41 -0700

    working on fig 8b haplotype/ LD plot

--- scripts/NESbigRR/14_Fig8b_LDplot.R
+++ scripts/NESbigRR/14_Fig8b_LDplot.R
@@ -51,13 +51,12 @@ HEM.topSNPsSM <- HEM.topSNPsSM[which(HEM.topSNPsSM$Pos > 344785),]
 HEM.topSNPsSM <- HEM.topSNPs[which(HEM.topSNPs$Pos < 355042),]
 HEM.topSNPsSM <- HEM.topSNPsSM[which(HEM.topSNPsSM$Pos > 337285),]
 
+#-------------------------------------------------------------------------
+#START HERE, ACTUALLY
 #now to draw that darn LD plot
 #this requires 3 file formats, none of which I have
 library("snp.plotter")
 
-
-#-------------------------------------------------------------------------
-#START HERE, ACTUALLY
 #this might make DNAbin files, from vcf files
 #let's try just a 4kb region on Chromosome 16
 #goes from POS 341 to POS 664510
@@ -65,5 +64,20 @@ library("snp.plotter")
 library("vcfR")
 vcf <- read.vcfR("data/genome/chr16_analysis/chr16_analysis_seg.recode.vcf", verbose = FALSE)
 my_dnabin1 <- vcfR2DNAbin(vcf, consensus = TRUE, extract.haps = FALSE, verbose = FALSE)
+my_dnabin1
+#plot it by site
+ape::image.DNAbin(my_dnabin1[,ape::seg.sites(my_dnabin1)])
+#save it as a fasta
+write.dna( my_dnabin1, file = 'data/genome/chr16_analysis/chr16_analysis_seg.fasta', format = 'fasta' )
 #this might make haplotype files, from DNAbin files
+#pegas fails on ubuntu -- run on windows PC
 library("pegas")
+my_haplo <- haplotype(my_dnabin1)
+plot(sort(my_haplo))
+attr(my_haplo, "index")[[2]]
+print(my_haplo)
+my_haplo_dist <- dist.haplotype.loci(my_haplo)
+?haplotype()
+
+subset(my_haplo, minfreq=20)
+as.data.frame(my_haplo)
