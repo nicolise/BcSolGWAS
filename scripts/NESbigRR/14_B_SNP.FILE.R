961e852c5e10ac61c34e0858e5a5b537058bf7f8
nicolise <ne.soltis@gmail.com>
2017-09-13 17:12:04 -0700

    still figure 8b work on linux

--- scripts/NESbigRR/14_B_SNP.FILE.R
+++ scripts/NESbigRR/14_B_SNP.FILE.R
@@ -1,8 +1,9 @@
 #Nicole E Soltis
-#081117
+#091317
 #plot of SNPs along gene of interest
+#and now: haplotype plots
 
-#12_singleGeneManhattan.R
+#14_B_SNP.FILE.R
 #---------------------------------------------
 rm(list=ls())
 setwd("~/Documents/GitRepos/BcSolGWAS/")
@@ -69,33 +70,6 @@ mySNP.FILE$SS.PVAL <- mySNP.FILE$LA1547
 
 mySNP.FILE <- mySNP.FILE[,c(21:24)]
 
-#-------------------------------------------------------------------------
-#START HERE, ACTUALLY
-#now to draw that darn LD plot
-#this requires 3 file formats, none of which I have
-library("snp.plotter")
+#write it
+write.table(mySNP.FILE, file="data/genome/chr16_analysis/SNP.FILE.tsv",quote=FALSE, sep='\t',col.names=TRUE,row.names=FALSE)
 
-#this might make DNAbin files, from vcf files
-#let's try just a 4kb region on Chromosome 16
-#goes from POS 341 to POS 664510
-#try POS 344785 to POS 347542
-library("vcfR")
-vcf <- read.vcfR("data/genome/chr16_analysis/chr16_analysis_seg.recode.vcf", verbose = FALSE)
-my_dnabin1 <- vcfR2DNAbin(vcf, consensus = TRUE, extract.haps = FALSE, verbose = FALSE)
-my_dnabin1
-#plot it by site
-ape::image.DNAbin(my_dnabin1[,ape::seg.sites(my_dnabin1)])
-#save it as a fasta
-write.dna( my_dnabin1, file = 'data/genome/chr16_analysis/chr16_analysis_seg.fasta', format = 'fasta' )
-#this might make haplotype files, from DNAbin files
-#pegas fails on ubuntu -- run on windows PC
-library("pegas")
-my_haplo <- haplotype(my_dnabin1)
-plot(sort(my_haplo))
-attr(my_haplo, "index")[[2]]
-print(my_haplo)
-my_haplo_dist <- dist.haplotype.loci(my_haplo)
-?haplotype()
-
-subset(my_haplo, minfreq=20)
-as.data.frame(my_haplo)
