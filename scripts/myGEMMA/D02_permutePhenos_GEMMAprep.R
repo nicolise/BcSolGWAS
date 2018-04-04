72c8ba017e25c5eea54d92fd6af1ca760c6fc3b8
nicolise <ne.soltis@gmail.com>
2018-04-04 16:59:15 -0700

    ready to run permutations! whee

--- scripts/myGEMMA/D02_permutePhenos_GEMMAprep.R
+++ scripts/myGEMMA/D02_permutePhenos_GEMMAprep.R
@@ -13,11 +13,11 @@ rm(list=ls())
 #2. copy plink executable to GEMMA_files
 #3. in command prompt: cd to GEMMA_files
 #4. RUN ./plink --noweb --file D_01_PLINK/dpbinMAF20NA10 --maf 0.2 --make-bed --out binMAF20NA10 } did this ONCE for all BcSolGWAS. NEXT STEP is customized by DWS/ 12phenos/ permutation
-#5. run this script (D01_permutePhenos_GEMMAprep.R)
+#5. run this script (D02_permutePhenos_GEMMAprep.R)
 #6. cd to GEMMA_files
-#7. copy edited .fam, .bim, .bam to D_02_randGEMMA/
-#8. calculate k-matrix with: bash randphenos_GEMMA_kmatrix.sh
-#9. run GEMMA: bash randphenos_GEMMA_kmatrix_run.sh
+#7. copy edited _rand.fam, .bim, .bam to D_02_randGEMMA/
+#8. calculate k-matrix with: bash fullrand_GEMMA_kmatrix.sh
+#9. run GEMMA: bash fullrand_GEMMA_kmatrix_run.sh
 
 #10: Pheno order is "V2"           "LA0410"       "LA0480"       "LA1547"       "LA1589"
 #"LA1684"       "LA2093"       "LA2176"       "LA2706"       "LA3008"      
@@ -49,9 +49,9 @@ intersect(myFAM_match$V2,Phenos_match$V2)
 
 #add an empty variables 1.01.12 and 1.02.19 and Triple7 to Phenos
 for (y in 2:length(Phenos_match)){
-  Phenos_match[96,y] <- mean(Phenos_match[,y], na.rm=TRUE)
-  Phenos_match[97,y] <- mean(Phenos_match[,y], na.rm=TRUE)
-  Phenos_match[98,y] <- mean(Phenos_match[,y], na.rm=TRUE)
+  Phenos_match[96,y] <- NA
+  Phenos_match[97,y] <- NA
+  Phenos_match[98,y] <- NA
 }
 levels(Phenos_match$V2) <- c(levels(Phenos_match$V2), "1.01.12", "1.02.19","Triple7", "MEAPGG")
 Phenos_match[96,1] <- "1.01.12"
@@ -69,7 +69,7 @@ Phenos_match$V2
 Phenos_match <- Phenos_match[c(1:5,94,6:15,95,16:44,93,45:91,96,92),]
 
 #double check now and match orders
-myFAM_match$delete <- c(1:97)
+myFAM_match$delete <- c(1:96)
 myFAM_match <- myFAM_match[ order(myFAM_match$V2), ]
 setdiff(myFAM_match$V2,Phenos_match$V2)
 setdiff(Phenos_match$V2,myFAM_match$V2)
@@ -77,11 +77,14 @@ intersect(myFAM_match$V2,Phenos_match$V2)
 myFAM_match <- myFAM_match[1:96,]
 
 #reorder phenotypes to make life way easier
-Phenos_match <- Phenos_match[,c()]
+names(Phenos_match)[13] <- "LA0480"
+names(Phenos_match)[10] <- "LA0410"
+Phenos_match <- Phenos_match[,c(1,10,13,2:9,11:12,14:16)]
 
 myFAM_match2 <- cbind(myFAM_match, Phenos_match)
-myFAM_match2 <- myFAM_match2[order(myFAM_match$delete),]
-myFAM_match2 <- myFAM_match2[,c(1:5,11:ncol(myFAM_match2))]
+myFAM_match2 <- myFAM_match2[order(myFAM_match2$delete),]
+myFAM_match2 <- myFAM_match2[,c(1:5,9:ncol(myFAM_match2))]
 
 write.csv(myFAM_match2, "GEMMA_files/D_02_randGEMMA/binMAF20NA10_fam.csv")
 write.table(myFAM_match2, "GEMMA_files/D_02_randGEMMA/binMAF20NA10_allphenos.fam", row.names=FALSE, col.names=TRUE)
+#copy _allphenos.fam to _rand.fam and also copy 01/.bed and 01/.bim to 02/_rand.
--- scripts/myGEMMA/D03_permutePhenos.R
+++ scripts/myGEMMA/D03_permutePhenos.R
@@ -1,22 +1,38 @@
 #Nicole E Soltis
 #R loop to run to generate randomized phenotypes, then feed into GEMMA
 #-------------------------------------------------------------------------
+rm(list=ls())
+setwd("~/Documents/GitRepos/BcSolGWAS/data")
+
+#pipeline: see D02_permutePhenos_GEMMAprep.R
+#have already run GEMMA for unpermuted phenotypes
+#here: looped permutations to generate randomized phenotype files
+#then: looped GEMMA across all files
+#then: looped value extractions
 
 #now randomize each phenotype! Wheee
-myFAM_match <- read.csv("GEMMA_files/D_02_randGEMMA/binMAF20NA10_fam.csv")
+Phenos_match <- read.csv("GEMMA_files/D_02_randGEMMA/binMAF20NA10_fam.csv")
 
 #test on a small run 
-for (i in 1:10){
-  Phenos_rand <- transform(Phenos_match, LA0410 = sample(LA410), LA0480 = sample(LA480), LA1547 = sample(LA1547), LA1589 = sample(LA1589), LA1684 = sample(LA1684), LA2093 = sample(LA2093), LA2176 = sample(LA2176), LA2706 = sample(LA2706), LA3008 = sample(LA3008), LA3475 = sample(LA3475), LA4345 = sample(LA4345), LA4355 = sample(LA4355),  Domesticated = sample(Domesticated), Wild = sample(Wild), DmWoD = sample(DmWoD))
-  Phenos_rand <- Phenos_rand[,-c(10,13)]
-  #only keep randomized phenotypes, not originals
-  Phenos_rand <- Phenos_rand[,c(1,15,16,2:14)]
+Sys.time()
+for (i in 1:1000){
+  Phenos_rand <- transform(Phenos_match, LA0410 = sample(LA0410), LA0480 = sample(LA0480), LA1547 = sample(LA1547), LA1589 = sample(LA1589), LA1684 = sample(LA1684), LA2093 = sample(LA2093), LA2176 = sample(LA2176), LA2706 = sample(LA2706), LA3008 = sample(LA3008), LA3475 = sample(LA3475), LA4345 = sample(LA4345), LA4355 = sample(LA4355),  Domesticated = sample(Domesticated), Wild = sample(Wild), DmWoD = sample(DmWoD))
+  #select columns
+  Phenos_rand <- Phenos_rand[,-c(1)]
+  newdir <- paste0("GEMMA_files/D_05_bigrand/rand1k_",i)
+  dir.create(newdir)
+  cwd <- getwd()
+  setwd(newdir)
+  write.table(Phenos_rand, "binMAF20NA10_rand.fam", row.names=FALSE, col.names=TRUE)
+  setwd(cwd)
+  #and now copy .bed and .bim over to new directory
+  plink.folder <- paste0(cwd,"/GEMMA_files/D_02_randGEMMA")
+  new.folder <- paste0(cwd,"/",newdir)
+  # find the files that you want
+  my.bed <- list.files(plink.folder, ".bed$",full.names=T)
+  my.bim <- list.files(plink.folder, ".bim$",full.names=T)
+  # copy the files to the new folder
+  file.copy(my.bed, new.folder)
+  file.copy(my.bim, new.folder)
 }
-
-
-#now add Phenos_match onto myFAM_match
-myFAM_match2 <- cbind(myFAM_match, Phenos_rand)
-myFAM_match2 <- myFAM_match2[order(myFAM_match$delete),]
-myFAM_match2 <- myFAM_match2[,c(1:5,11:ncol(myFAM_match2))]
-
-write.table(myFAM_match2, "GEMMA_files/D_02_randGEMMA/binMAF20NA10_randphenos.fam", row.names=FALSE, col.names=TRUE)
+Sys.time()
