ecb388bf351a00c8e45205d6f3c1013fe36394f7
nicolise <ne.soltis@gmail.com>
2018-05-09 15:46:59 -0700

    adding tomato genetic distances to ms

--- scripts/myGEMMA/D14_SolGeneDist.R
+++ scripts/myGEMMA/D14_SolGeneDist.R
@@ -27,3 +27,57 @@ setwd("~/Projects/BcSolGWAS")
 jpeg("paper/plots/addGEMMA/SolDistance.jpg", width=10, height=10, units='in', res=600)
 heatmap(blah)
 dev.off()
+
+#-----------------------------------------------------------------------------------------------------------
+#here's another option : Sim 2012 
+rm(list=ls())
+setwd("~/Projects/BcSolGWAS/data")
+mysim <- read.csv("SlGenome/Sim2012/TableS2_NES.csv")
+
+library("poppr")
+library("pegas")
+
+#remove row 2 (key for my tomato dataset)
+mygenotypes_sim <- mysim[c(1),]
+mysim <- mysim[-c(1),]
+
+#create a matrix with only genotype data
+mysim$Position <- as.numeric(mysim$Position..Mbp.) * 1000
+mysim$Locus <- paste(mysim$Chromosome, mysim$Position, sep=".")
+mygenosim <- mysim[,c(433,6:431)]
+Bcgenos.domest <- c("LA0410","LA2706","LA3008","LA3475","LA4345","LA4355","Gold.Nugget","Heinz.1706","M82","Moneymaker","San.Marzano")
+Bcgenos.wild <- c("LA0480","LA1547","LA1589","LA1684","LA2093","LA2176")
+
+
+
+#transpose
+myloci <- mygenosim[,1]
+mygenosim <- mygenosim[,2:427]
+mygenosim_t <- as.data.frame(t(mygenosim))
+names(mygenosim_t) <- myloci
+
+ind <- as.character(names(mygenosim)) # individual labels 
+
+myGenind <- df2genind(mygenosim_t, ploidy = 2, ind.names = ind, sep="")
+myGenind2 <- genind2loci(myGenind)
+
+#Euclidean distance in {adegenet}
+distgenEUCL <- dist(myGenind, method = "euclidean", diag = FALSE, upper = FALSE, p = 1)
+hist(distgenEUCL)
+blah <- data.matrix(distgenEUCL)
+
+library(gplots)
+#color code names
+mycolor.samples <- as.data.frame(ind)
+mycolor.samples$colors <- ifelse(mycolor.samples$ind %in% Bcgenos.domest, "Blue", ifelse(mycolor.samples$ind %in% Bcgenos.wild, "Orange","Black"))
+sample.types <- mycolor.samples$colors
+setwd("~/Projects/BcSolGWAS")
+jpeg("paper/plots/addGEMMA/SolDistance_Sim2012.jpg", width=30, height=30, units='in', res=600)
+heatmap.2(blah, trace="none", colCol=sample.types)
+dev.off()
+
+#or just the dendrogram
+setwd("~/Projects/BcSolGWAS")
+jpeg("paper/plots/addGEMMA/SolDistance_dendro_Sim2012.jpg", width=10, height=10, units='in', res=600)
+plot(hclust(distgenEUCL))
+dev.off
